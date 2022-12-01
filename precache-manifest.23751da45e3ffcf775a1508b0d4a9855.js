1882f07577d0bc0800c971ff2cc3ccf9eeb97b89
Ramanie <thilak.ramanie@siemens.com>
2022-12-01 17:19:47 +0530

    Updates

---
+++ precache-manifest.23751da45e3ffcf775a1508b0d4a9855.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06b5cc4475bc3857834d59615454a0b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d71da4713d992dcb00a",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "dfc82ce60613cacb7c47",
+    "url": "./static/css/main.c191bd7f.chunk.css"
+  },
+  {
+    "revision": "5d71da4713d992dcb00a",
+    "url": "./static/js/2.cb3c790b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.cb3c790b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc82ce60613cacb7c47",
+    "url": "./static/js/main.47207e12.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
