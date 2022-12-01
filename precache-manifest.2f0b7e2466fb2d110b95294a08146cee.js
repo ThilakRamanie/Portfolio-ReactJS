754983f504a68763268fb13b1939327c27d5269f
Ramanie <thilak.ramanie@siemens.com>
2022-12-01 17:24:22 +0530

    Updates

---
+++ precache-manifest.2f0b7e2466fb2d110b95294a08146cee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9a2fd5b91372b127683e40785c2d89",
+    "url": "/%7Brepository-name%7D/index.html"
+  },
+  {
+    "revision": "5d71da4713d992dcb00a",
+    "url": "/%7Brepository-name%7D/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d3d33fef48afaf1e0fce",
+    "url": "/%7Brepository-name%7D/static/css/main.c191bd7f.chunk.css"
+  },
+  {
+    "revision": "5d71da4713d992dcb00a",
+    "url": "/%7Brepository-name%7D/static/js/2.cb3c790b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/%7Brepository-name%7D/static/js/2.cb3c790b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d33fef48afaf1e0fce",
+    "url": "/%7Brepository-name%7D/static/js/main.a05cba51.chunk.js"
+  },
+  {
+    "revision": "ab092184fec7db749051",
+    "url": "/%7Brepository-name%7D/static/js/runtime-main.481eee77.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.23751da45e3ffcf775a1508b0d4a9855.js"
+  "/%7Brepository-name%7D/precache-manifest.2f0b7e2466fb2d110b95294a08146cee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Brepository-name%7D/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
