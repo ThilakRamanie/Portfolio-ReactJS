e4f17638c3855ea416e0dd85e9885e9cd3be89b7
ThilakRamanie <thilakramanie007@gmail.com>
2024-01-13 15:33:57 +0000

    Trying deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-ReactJS/",
   plugins: [react()],
-  build: {
-    outDir: './build'
-  }
 })
